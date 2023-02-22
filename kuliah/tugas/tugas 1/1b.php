4a6675f1b8ac5ce46395bc3581266169f2249d99
akbarabdimuhammad <125510670+akbarabdimuhammad@users.noreply.github.com>
2023-02-23 02:14:26 +0800

    komit pertama

---
+++ kuliah/tugas/tugas 1/1b.php
@@ -0,0 +1,29 @@
+<?php 
+$l ='29' 
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        saya adalah angka <b><?php echo $l ?></b>
+    </p>
+    <p>
+        jika aku di kali 5, maka aku sekarang menjadi <b><?php echo $l*5 ?></b>
+    </p>
+    <p>
+        jika aku di bagi 2, maka aku sekarang menjadi <b><?php echo $l*5/2 ?></b>
+    </p>
+    <p>
+        jika aku di tambah 75, maka aku sekarang menjadi <b><?php echo $l*5/2+75 ?></b>
+    </p>
+    <p>
+        jika aku di kurang 20, maka aku sekarang menjadi <b><?php echo $l*5/2+75-20 ?></b>
+    </p>
+</body>
+</html>
